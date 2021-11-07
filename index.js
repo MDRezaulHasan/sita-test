8716a56a6780ba7ca1276a07c6a3d1968c216b37
Md Rezaul Hasan <rezaul@Mds-MacBook-Pro.local>
2021-11-07 08:18:53 +0100

    Backend deployment

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express")
+const cors = require("cors")
+const morgan = require("morgan")
+const lowDB = require("lowdb")
+const productsRouter= require("./productRoutes")
+
+//for swagger UI
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
+
+const PORT = process.env.PORT || 4000;
+
+const FileSync = require("lowdb/adapters/FileSync")
+const adapter = new FileSync("db.json")
+const db = lowDB(adapter)
+
+db.defaults({ products: []}).write()
+
+//declaretion of swagger header
+const options = {
+	definition: {
+		openapi: "3.0.0",
+		info: {
+			title: "api-test",
+			version: "1.0.0",
+			description: "Demo project of rest api using nodejs and express hosted by Stockholm IT Academy (SITA)",
+		},
+		servers: [
+			{
+				url: "http://localhost:4000",
+			},
+		],
+	},
+	apis: ["./*.js"],
+};
+const specs = swaggerJsDoc(options);
+const app = express()
+
+app.use("/", swaggerUI.serve, swaggerUI.setup(specs));
+
+app.db = db;
+app.use(cors())
+app.use(express.json())
+app.use(morgan("dev"))
+app.use("/products",productsRouter)
+
+app.listen(PORT,()=>console.log(`Server is running in port: ${PORT}`))
---
+++ productRoutes.js
@@ -0,0 +1,195 @@
+const express = require("express")
+const router = express.Router()
+const {nanoid} = require("nanoid")
+const idLength = 8;
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Product:
+ *       type: object
+ *       required:
+ *         - name
+ *         - catagory
+ *         - price
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the book
+ *         name:
+ *           type: string
+ *           description: The product name
+ *         catagory:
+ *           type: string
+ *           description: The catagory name
+ *         price:
+ *           type: string
+ *           description: Price of the product
+ *       example:
+ *         id: d5fE_asz
+ *         name: Iphone 11
+ *         catagory: Mobile
+ *         price: 2000 
+ */
+
+/**
+ * @swagger
+ * /products:
+ *   get:
+ *     summary: Returns the list of all the products
+ *     tags: [Products]
+ *     responses:
+ *       200:
+ *         description: The list of the products
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Product'
+ */
+router.get("/", (req,res)=>{
+    const products = req.app.db.get("products")
+    res.send(products)
+})
+
+/**
+ * @swagger
+ * /products/{id}:
+ *   get:
+ *     summary: Get the book by id
+ *     tags: [Product]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The product id
+ *     responses:
+ *       200:
+ *         description: The product description by id
+ *         contens:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Product'
+ *       404:
+ *         description: The book was not found
+ */
+router.get("/:id", (req,res)=>{
+    const product = req.app.db.get("products").find({id:req.params.id}).value()
+    res.send(product)
+})
+
+/**
+ * @swagger
+ * /products:
+ *   post:
+ *     summary: Create a new book
+ *     tags: [Products]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Product'
+ *     responses:
+ *       200:
+ *         description: Product was successfully created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Product'
+ *       500:
+ *         description: Some server error
+ */
+
+router.post("/", (req,res)=>{
+    try{
+        const product ={
+            id:nanoid(idLength),
+            ...req.body
+        }
+        req.app.db.get("products").push(product).write();
+        res.send(product)
+
+    }catch(error){
+        return res.status(500).send(error)
+    }
+})
+
+/**
+ * @swagger
+ * /products/{id}:
+ *  put:
+ *    summary: Update the product by the id
+ *    tags: [Products]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: The product id
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Product'
+ *    responses:
+ *      200:
+ *        description: The product was updated
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Product'
+ *      404:
+ *        description: The product was not found
+ *      500:
+ *        description: Server error happened
+ */
+
+router.put("/:id", (req, res) => {
+	try {
+		req.app.db
+			.get("products")
+			.find({ id: req.params.id })
+			.assign(req.body)
+			.write();
+
+		res.send(req.app.db.get("products").find({ id: req.params.id }));
+	} catch (error) {
+		return res.status(500).send(error);
+	}
+});
+
+/**
+ * @swagger
+ * /products/{id}:
+ *   delete:
+ *     summary: Remove the product by id
+ *     tags: [Products]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The product id
+ * 
+ *     responses:
+ *       200:
+ *         description: The product was deleted
+ *       404:
+ *         description: The product was not found
+ */
+router.delete("/:id", (req, res) => {
+    console.log("Id: "+req.params.id.toString())
+	req.app.db.get("products").remove({ id: req.params.id.toString()}).write();
+
+	res.sendStatus(200);
+});
+
+module.exports = router;
